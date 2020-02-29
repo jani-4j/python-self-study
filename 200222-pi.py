b947a0e32ff45f652e31233b7f1d6e525d0f253a
Damian  Januszek <damian.januszek@v-it.pl>
2020-02-29 11:33:08 +0100

    first commit

---
+++ 200222-pi.py
@@ -0,0 +1,21 @@
+bokA = 10
+bokB = 14
+bokH = 6 
+PromienOkregu = 5
+WartoscPI = 3.14
+
+#w tym zadaniu liczymy pole kola
+PoleKola = (WartoscPI * PromienOkregu**2)
+print (PoleKola)
+
+#Obwod okregu 2 pi r 
+PoleOkregu = (2 * WartoscPI * PromienOkregu)
+print (PoleOkregu)
+
+#Pole prostokata
+PoleProstokata = (bokA * bokB)
+print (PoleProstokata)
+
+#Pole trapezu
+PoleTrapezu = ((bokA + bokB)*bokH)/2
+print ("Pole trapezu =",PoleTrapezu)
---
+++ 200229-compare.py
@@ -0,0 +1,4 @@
+#porownywanie zmiennych
+suma1 = 1+3
+suma2 = 2+2
+print (suma1 == suma2)
